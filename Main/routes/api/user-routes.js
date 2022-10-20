64160d7bf92b86aa78ad4d46ca5366c2e920a9c8
Sterling Carfrae <scarfrae@Sterlings-MacBook-Pro.local>
2022-10-19 23:53:02 -0700

    working on routes

---
+++ Main/routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
+
+// **`/api/users/:userId/friends/:friendId`**
+
+// * `POST` to add a new friend to a user's friend list
+
+// * `DELETE` to remove a friend from a user's friend list

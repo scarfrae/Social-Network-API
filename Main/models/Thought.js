780a3ee03fcee40fc89fb6200a8e89dddefb8e39
Sterling Carfrae <scarfrae@Sterlings-MacBook-Pro.local>
2022-10-20 00:34:52 -0700

    working on models

---
+++ Main/models/Thought.js
@@ -0,0 +1,33 @@
+//think this is slightly wrong
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    assignments: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
